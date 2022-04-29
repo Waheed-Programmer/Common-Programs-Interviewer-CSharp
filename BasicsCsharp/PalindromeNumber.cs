256795b8c3ce3daf6f35ffd823977dcea0daee15
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-04-29 11:37:12 +0500

    make a program to check number are palindrom or not

---
+++ BasicsCsharp/PalindromeNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class PalindromeNumber
+    {
+        static void Main(string[] args)
+        {
+            int num, temp, rem, rev=0;
+            Console.WriteLine("Enter Integer");
+            num= Convert.ToInt32(Console.ReadLine());
+            temp = num;
+            while (num > 0)
+            {
+                rem = num % 10;
+                rev = rev*10 + rem;
+                num = num / 10;
+
+            }
+            if (rev == temp)
+            {
+                Console.WriteLine("Number are Palindram");
+            }
+            else
+            {
+                Console.WriteLine("Nuber Are not Palindrom");
+            }
+
+        }
+    }
+}
--- BasicsCsharp/RemoveDuplicateArrayElement.cs
+++ BasicsCsharp/RemoveDuplicateArrayElement.cs
@@ -13,7 +13,7 @@ namespace BasicsCsharp
             int[] arr = { 1, 2, 3, 4, 5, 5, 4, 6 };
             int[] dist = arr.Distinct().ToArray();
             Array.ForEach(dist, v => Console.WriteLine(v));
-
+            // Remove String Element in array
             string[] str = { "cat", "dog", "cat", "animal", "parrort", "dog" };
             string[]com= str.Distinct().ToArray();
             Array.ForEach(com, vi => Console.WriteLine(vi));
