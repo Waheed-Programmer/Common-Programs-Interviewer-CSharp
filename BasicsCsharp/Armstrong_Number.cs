04f845a160984d1b98834edb398a1fc1f820c183
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-04-29 10:19:27 +0500

    Initial commit

---
+++ BasicsCsharp/Armstrong_Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    public class Armstrong_Number
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
