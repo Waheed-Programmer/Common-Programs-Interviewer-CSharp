f7b1c733690f750ca2b2e58ba6486a3258520f8f
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-04-29 11:16:27 +0500

    remove duplicate element from arrray

---
+++ BasicsCsharp/RemoveDuplicateArrayElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class RemoveDuplicateArrayElement
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 5, 4, 6 };
+            int[] dist = arr.Distinct().ToArray();
+            Array.ForEach(dist, v => Console.WriteLine(v));
+
+            string[] str = { "cat", "dog", "cat", "animal", "parrort", "dog" };
+            string[]com= str.Distinct().ToArray();
+            Array.ForEach(com, vi => Console.WriteLine(vi));
+        }
+    }
+}
