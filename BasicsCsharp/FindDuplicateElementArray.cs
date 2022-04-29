74997508427bbab5b38a40f5e20d6c583bf69d1a
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-04-29 10:57:39 +0500

    Implement to find duplicate element in array

---
+++ BasicsCsharp/FindDuplicateElementArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class FindDuplicateElementArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 6, 3, };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if(arr[i] == arr[j])
+                    {
+                        Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+
+        }
+    }
+}
