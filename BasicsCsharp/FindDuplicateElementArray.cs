365160b725ea39f2e1300ce254b7cefaecc88ec5
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-04-29 12:02:32 +0500

    remove all duplicate string item

--- BasicsCsharp/FindDuplicateElementArray.cs
+++ BasicsCsharp/FindDuplicateElementArray.cs
@@ -13,11 +13,24 @@ namespace BasicsCsharp
             int[] arr = { 1, 2, 3, 4, 5, 6, 6, 3, };
             for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = i+1; j < arr.Length; j++)
+                for (int j = i + 1; j < arr.Length; j++)
                 {
-                    if(arr[i] == arr[j])
+                    if (arr[i] == arr[j])
                     {
                         Console.WriteLine(arr[i]);
+                    }
+                }
+            }
+            //Find string duplicates using toCharArray() method
+            string str = "aaetioommrl";
+            char[] ch = str.ToCharArray();
+            for (int w = 0; w < ch.Length; w++)
+            {
+                for (int y = w + 1; y < ch.Length; y++)
+                {
+                    if (ch[w] == ch[y])
+                    {
+                        Console.WriteLine(ch[w]);
                     }
                 }
             }
--- BasicsCsharp/RemoveDuplicateArrayElement.cs
+++ BasicsCsharp/RemoveDuplicateArrayElement.cs
@@ -13,10 +13,21 @@ namespace BasicsCsharp
             int[] arr = { 1, 2, 3, 4, 5, 5, 4, 6 };
             int[] dist = arr.Distinct().ToArray();
             Array.ForEach(dist, v => Console.WriteLine(v));
+            Console.WriteLine("=================================>");
             // Remove String Element in array
             string[] str = { "cat", "dog", "cat", "animal", "parrort", "dog" };
             string[]com= str.Distinct().ToArray();
             Array.ForEach(com, vi => Console.WriteLine(vi));
+
+            Console.WriteLine("=================================>");
+
+
+            //Remove duplicate element in string
+            string myStr = "aaetioommrl";
+            var unique = new HashSet<char>(myStr);
+            foreach (var item in unique)
+            Console.WriteLine(item);
+
         }
     }
 }
