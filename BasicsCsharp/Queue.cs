7357c1fdb06ee2090688583a1a6b14e87e9f7649
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-05-04 09:29:47 +0500

    implementations of queue in c#

---
+++ BasicsCsharp/Queue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class Queue
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(12);
+            q.Enqueue(15);
+            q.Enqueue(19);
+            q.Enqueue(14);
+
+            Console.WriteLine("The List of Queue is " +q.Count());
+
+            q.Dequeue();
+            q.Dequeue();
+
+            Console.WriteLine("After Deque the List of Queue is " + q.Count());
+
+        }
+    }
+}
