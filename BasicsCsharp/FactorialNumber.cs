40d600141197dc8ff858a28e85ef6dc63ff43ffe
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-05-04 10:21:51 +0500

    Find factorials number

---
+++ BasicsCsharp/FactorialNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class FactorialNumber
+    {
+        static void Main(string[] args)
+        {
+            int num, fact = 1;
+            Console.WriteLine("Eneter Integer");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <=num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("The Factorial of " + num + " Is " + fact);
+        }
+    }
+}
