313cb77c9d3727e674f41061b2dde8b7a97dace1
WAHEED ARSHAD <70683576+Waheed-Programmer@users.noreply.github.com>
2022-05-15 06:40:11 +0500

    Create a new class and implement a code in which apply reverse of a string

---
+++ BasicsCsharp/ReverseString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicsCsharp
+{
+    internal class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string str, revers = "";
+            Console.WriteLine("Enter String");
+            str = Console.ReadLine();
+            str = str.ToLower();
+            for (int i = str.Length-1; i>=0; i--)
+            {
+                revers = revers + str[i];
+            }
+            Console.WriteLine("The String is {0}:",revers);
+        }
+    }
+}
